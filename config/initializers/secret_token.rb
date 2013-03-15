97afaa46791176faecee140248f6c5e53c1bf26d
vagrant <vagrant@precise32.(none)>
2013-03-15 10:17:08 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twt::Application.config.secret_token = '85f4486ff29dde0bae5b526570f8bbc52d8ef3576241668edbffc5eb0f681ab3d192e768b1cb4907535feb723e15335ea79d9e92f7ffa80d26ab955dfb940c74'
